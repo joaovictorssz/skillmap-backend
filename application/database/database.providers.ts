59be6e01df1b32a78ac787776a569402106bfd08
João Victor Silva <joaovictorsilva.dev@gmail.com>
2023-05-22 00:57:27 -0400

    fix: mongodb url to atlas

--- application/database/database.providers.ts
+++ application/database/database.providers.ts
@@ -4,5 +4,5 @@ import * as mongoose from 'mongoose'
 
 export const  databaseProviders  = [{
     provide: "DATABASE_CONNECTION",
-    useFactory: (): Promise<typeof mongoose> => mongoose.connect('mongodb://0.0.0.0:27017/skillmap')
+    useFactory: (): Promise<typeof mongoose> => mongoose.connect('mongodb+srv://skillmap_app:1234@cluster0.1qs8bli.mongodb.net/?retryWrites=true&w=majority')
 }]
